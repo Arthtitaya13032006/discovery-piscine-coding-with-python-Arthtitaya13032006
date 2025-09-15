c163b450a3bb630dee3974522e7101a8c39d8ac1
Arthtitaya13032006 <67030260@kmitl.ac.th>
2025-09-15 21:58:37 +0700

    commit

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,10 @@
+while True:
+    # Prompt the user for input
+    user_input = input("What you gotta say? : ")
+
+    # If the user types "STOP", break the loop
+    if user_input == "STOP":
+        break
+
+    # Print the response and continue the loop
+    print("I got that! Anything else? :")
